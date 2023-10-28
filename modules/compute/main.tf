865f0142da3bb465ee0a744fc26e10cd407445a5
hjk08 <62510981+hjk08@users.noreply.github.com>
2023-10-28 13:55:04 +0200

    instance name

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -13,7 +13,7 @@ data "openstack_networking_secgroup_v2" "default" {
 }
 
 resource "openstack_compute_instance_v2" "instance" {
-  name            = "nieco" //"${var.instance_name}"
+  name            = "${var.instance_name}"
   image_id        = data.openstack_images_image_v2.image_1.id
   flavor_id       = data.openstack_compute_flavor_v2.flavor_1.id
   key_pair        = var.key_pair_name
--- project/main.tf
+++ project/main.tf
@@ -5,7 +5,7 @@ data "http" "my_public_ip" {
 }
 
 resource "openstack_compute_keypair_v2" "keypair" {
-  name       = "ONPK1"
+  name       = "ONPK1-key"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDEc3eVfn/wjRcWWEoZjBXiCW5lwrLh2A11fuPZcKl1GSAg5OtLSKacJXHjKGLnuJVqFPxiLT4A0OPc9A0gk8TRueNi7mfauKiHjlldcI2oF9ueMyyhBvyHtcsHftG4iUJ4Za3KvsDwXM1UdRFO17tgxXrwcYhe0T1wYIbeLvO8L+Vembec+sp81sL8UM7jjc94hve1JP6j2B91/i+s8UecZqTx5cvFHZghRk8BpsD62CWGEosbYOb9iqq29NuypJrQzScEjUnnZ2AAi9iNaebqn8ECdOOXv7NqOEVOBG0xPZtoH0DKlIUYS9uVQXLNsHIqfOIpeckVfb/fVSCEz4lr"
 }
 
