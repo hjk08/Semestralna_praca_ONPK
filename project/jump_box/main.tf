c5fc63b0513bbfa49875f693f598cca81da6c8af
hjk08 <62510981+hjk08@users.noreply.github.com>
2023-11-01 13:44:16 +0100

    test

--- project/jump_box/main.tf
+++ project/jump_box/main.tf
@@ -4,6 +4,10 @@ data "http" "my_public_ip" {
   url = "http://ipv4.icanhazip.com"
 }
 
+data "http" "remote_script" {
+  url = "https://raw.githubusercontent.com/hjk08/Semstralna_praca_ONPK/project/jump_box/scripts/base.sh"
+}
+
 resource "openstack_compute_keypair_v2" "keypair" {
   name       = "ONPK1-key"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDEc3eVfn/wjRcWWEoZjBXiCW5lwrLh2A11fuPZcKl1GSAg5OtLSKacJXHjKGLnuJVqFPxiLT4A0OPc9A0gk8TRueNi7mfauKiHjlldcI2oF9ueMyyhBvyHtcsHftG4iUJ4Za3KvsDwXM1UdRFO17tgxXrwcYhe0T1wYIbeLvO8L+Vembec+sp81sL8UM7jjc94hve1JP6j2B91/i+s8UecZqTx5cvFHZghRk8BpsD62CWGEosbYOb9iqq29NuypJrQzScEjUnnZ2AAi9iNaebqn8ECdOOXv7NqOEVOBG0xPZtoH0DKlIUYS9uVQXLNsHIqfOIpeckVfb/fVSCEz4lr"
@@ -16,9 +20,8 @@ data "cloudinit_config" "user_data"{
   part {
     content_type = "text/x-shellscript"
     filename     = "network_set_up"
-    content      = file("github.com/hjk08/Semstralna_praca_ONPK/project/jump_box/scripts/base.sh")
+    content      = data.http.remote_script.body
   }
-
 }
 
 module "network" {
