27472cd4d1de0af6de2c5b6b17216466d9c445e2
hjk08 <62510981+hjk08@users.noreply.github.com>
2023-11-01 13:58:26 +0100

    fix

--- project/private_server/main.tf
+++ project/private_server/main.tf
@@ -13,6 +13,17 @@ resource "openstack_compute_keypair_v2" "keypair" {
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDEc3eVfn/wjRcWWEoZjBXiCW5lwrLh2A11fuPZcKl1GSAg5OtLSKacJXHjKGLnuJVqFPxiLT4A0OPc9A0gk8TRueNi7mfauKiHjlldcI2oF9ueMyyhBvyHtcsHftG4iUJ4Za3KvsDwXM1UdRFO17tgxXrwcYhe0T1wYIbeLvO8L+Vembec+sp81sL8UM7jjc94hve1JP6j2B91/i+s8UecZqTx5cvFHZghRk8BpsD62CWGEosbYOb9iqq29NuypJrQzScEjUnnZ2AAi9iNaebqn8ECdOOXv7NqOEVOBG0xPZtoH0DKlIUYS9uVQXLNsHIqfOIpeckVfb/fVSCEz4lr"
 }
 
+data "cloudinit_config" "user_data"{
+  gzip          = false
+  base64_encode = false
+
+  part {
+    content_type = "text/x-shellscript"
+    filename     = "network_set_up"
+    content      = data.http.remote_script.body
+  }
+}
+
 data "openstack_networking_network_v2" "private_network" {
   name = "local_private"
 }
