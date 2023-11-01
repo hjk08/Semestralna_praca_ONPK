da2c358f7199b4765cf0d8b6b9a3f6278ab5a803
hjk08 <62510981+hjk08@users.noreply.github.com>
2023-11-01 14:02:49 +0100

    fix

--- project/private_server/main.tf
+++ project/private_server/main.tf
@@ -4,10 +4,6 @@ data "http" "my_public_ip" {
   url = "http://ipv4.icanhazip.com"
 }
 
-data "http" "remote_script" {
-  url = "https://github.com/hjk08/Semstralna_praca_ONPK/blob/33406bcdc76e7c365a23fd7904b9cd61136d63df/project/private_server/scripts/base.sh"
-}
-
 resource "openstack_compute_keypair_v2" "keypair" {
   name       = "ONPK2-key"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDEc3eVfn/wjRcWWEoZjBXiCW5lwrLh2A11fuPZcKl1GSAg5OtLSKacJXHjKGLnuJVqFPxiLT4A0OPc9A0gk8TRueNi7mfauKiHjlldcI2oF9ueMyyhBvyHtcsHftG4iUJ4Za3KvsDwXM1UdRFO17tgxXrwcYhe0T1wYIbeLvO8L+Vembec+sp81sL8UM7jjc94hve1JP6j2B91/i+s8UecZqTx5cvFHZghRk8BpsD62CWGEosbYOb9iqq29NuypJrQzScEjUnnZ2AAi9iNaebqn8ECdOOXv7NqOEVOBG0xPZtoH0DKlIUYS9uVQXLNsHIqfOIpeckVfb/fVSCEz4lr"
@@ -20,7 +16,7 @@ data "cloudinit_config" "user_data"{
   part {
     content_type = "text/x-shellscript"
     filename     = "network_set_up"
-    content      = data.http.remote_script.body
+    content      = file("${path.module}/scripts/base.sh")
   }
 }
 
