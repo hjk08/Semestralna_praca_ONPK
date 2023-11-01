8d4f404a3a728d56e57f2130adf3a2b4cf04cdca
hjk08 <62510981+hjk08@users.noreply.github.com>
2023-11-01 11:32:31 +0100

    key

--- project/private_server/main.tf
+++ project/private_server/main.tf
@@ -4,6 +4,11 @@ data "http" "my_public_ip" {
   url = "http://ipv4.icanhazip.com"
 }
 
+resource "openstack_compute_keypair_v2" "keypair" {
+  name       = "ONPK2-key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDEc3eVfn/wjRcWWEoZjBXiCW5lwrLh2A11fuPZcKl1GSAg5OtLSKacJXHjKGLnuJVqFPxiLT4A0OPc9A0gk8TRueNi7mfauKiHjlldcI2oF9ueMyyhBvyHtcsHftG4iUJ4Za3KvsDwXM1UdRFO17tgxXrwcYhe0T1wYIbeLvO8L+Vembec+sp81sL8UM7jjc94hve1JP6j2B91/i+s8UecZqTx5cvFHZghRk8BpsD62CWGEosbYOb9iqq29NuypJrQzScEjUnnZ2AAi9iNaebqn8ECdOOXv7NqOEVOBG0xPZtoH0DKlIUYS9uVQXLNsHIqfOIpeckVfb/fVSCEz4lr"
+}
+
 module "jump_box" {
   source      = "github.com/hjk08/Semstralna_praca_ONPK/project/jump_box" 
   username    = var.username
@@ -16,7 +21,7 @@ module "instance1" {
   instance_name         = "private_server"
   sec_group_name        = "server_sec_gr"
   static_ip             = "10.10.10.43"
-  keypair               = module.jump_box.keypair_name
+  keypair               = openstack_compute_keypair_v2.keypair.name
   my_public_ip          = data.http.my_public_ip.response_body
   private_network_name  = module.jump_box.private_network_name
 }
